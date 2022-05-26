9495a6a401cfcbd416caefa67e2da645e9bf499a
Gabrielamb2 <gabrielamb2@al.insper.edu.br>
2022-05-26 20:29:27 -0300

    Adicionando transicao de cenas

---
+++ ProjetoFinal/Assets/_Scripts/Transicao.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transicao : MonoBehaviour
+{
+   public Animator transition;
+   public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            LoadNextScene();
+        }
+        
+    }
+    public void LoadNextScene(){
+
+        StartCoroutine(LoadSceneTransition(SceneManager.GetActiveScene().buildIndex + 1));
+       
+    }
+
+    IEnumerator LoadSceneTransition(int sceneIndex){
+       transition.SetTrigger("Start");
+
+       yield return new WaitForSeconds(transitionTime);
+
+       SceneManager.LoadScene(sceneIndex);
+    }
+}
