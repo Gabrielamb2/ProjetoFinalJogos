c6957f98ad84221dfb335a265213365bbd7d531c
gabriellaec <gabriellaec@al.insper.edu.br>
2022-05-26 21:46:42 -0300

    quarto andar funciona yayyy

--- ProjetoFinal/Assets/_Scripts/Player.cs
+++ ProjetoFinal/Assets/_Scripts/Player.cs
@@ -6,12 +6,13 @@ public class Player : MonoBehaviour
 {
     private Animator anim;   
     // GameManager gm;
-    public float velocidade;
+    private float velocidade;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        velocidade = 6;  
+        velocidade = 3;  
+
         // gm = GameManager.GetInstance();
     }
 
@@ -29,14 +30,20 @@ public class Player : MonoBehaviour
         pos.x = Mathf.Clamp((float)pos.x, (float)0.04, (float)0.96);
         transform.position = Camera.main.ViewportToWorldPoint(pos);
 
-        // if (yInput != 0 || xInput != 0)
-        // {
-        //     anim.SetFloat("Velocity", 1.0f);
-        // }
-        // else
-        // {
-        //     anim.SetFloat("Velocity", 0.0f);
-        // }
+        float screen_size=4.0f;
+        if (transform.position.y <= -screen_size)
+            transform.position = new Vector3(transform.position.x, -screen_size, transform.position.z);
+
+        if (inputY != 0 || inputX != 0)
+        {
+            anim.SetFloat("Velocity", 1.0f);
+            // Debug.Log("Walking\n");
+        }
+        else
+        {
+            anim.SetFloat("Velocity", 0.0f);
+            // Debug.Log("Idle\n");
+        }
 
        
     //    if(Input.GetKeyDown(KeyCode.Escape) && gm.gameState == GameManager.GameState.GAME) {
@@ -52,14 +59,9 @@ public class Player : MonoBehaviour
 
     // private void OnTriggerEnter2D(Collider2D collision)
     // {
-    //     if (collision.CompareTag("Inimigos"))
+    //     if (collision.CompareTag("Escorregador") || (collision.CompareTag("Elevador"))
     //     {
         
-    //     }
-
-    //     else if (collision.CompareTag("Nave"))
-    //     {
-           
     //     }
 
      
